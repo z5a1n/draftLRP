673a4986b2b6b1dcdb519622e265ac8522a2fa34
Barrett <Tim.Barrett@dfo-mpo.gc.ca>
2021-11-25 16:31:26 -0400

    11/25

--- Exercise 1/ex1.R
+++ Exercise 1/ex1.R
@@ -1,15 +1,15 @@
 ########################################################################################################################
-# Exercise 1: Plot of Landings by MU and plots of indices with example moving averages and loess smoothers
+# Exercise 1: Plot of Catch by MU and plots of indices with example moving averages and loess smoothers
 ########################################################################################################################
 
 rm(list=ls())
 library(ggplot2)
 
 #Read in data inputs
-L <- read.csv(paste0(getwd(),"/Exercise 1/ex1_landings.csv"))
+C <- read.csv(paste0(getwd(),"/Exercise 1/ex1_catch.csv"))
 D <- read.csv(paste0(getwd(),"/Exercise 1/ex1_indices.csv"))
 
-head(L)
+head(C)
 tail(D)
 
 #Calculate CPUE for purse seine fleet in SWNS/BoF
@@ -19,8 +19,9 @@ D$PS_CPUE <- (D$PS_Catch_MU1)/D$PS_Effort
 # Plots (entire stock area)
 ########################################################################################################################
 
-# 1. landings by MU
-ggplot() + geom_area(data=L, mapping=aes(y=Landings_kt,x=Year,fill=MU)) + theme_classic() + labs(x='Year', y="Landings (kt)") + scale_x_continuous(limits = c(0,50), expand = c(0, 0)) + scale_y_continuous(limits = c(0,300), expand = c(0, 0)) 
+# 1. Catch by MU
+ggplot() + geom_area(data=C, mapping=aes(y=Catch_kt,x=Year,fill=MU)) + theme_classic() + labs(x='Year', y="Total Catch (kt)") + scale_x_continuous(limits = c(0,50), expand = c(0, 0)) + scale_y_continuous(limits = c(0,300), expand = c(0, 0)) + scale_fill_manual(values=c("lightskyblue2","plum3","lightgoldenrod"))
+ 
 
 # 2. MU1 and MU2 bottom trawl survey with an example moving average and loess smoother if required
 
@@ -60,14 +61,14 @@ ggplot(D[!is.na(D$Ac_Index_MU1_3),]) + geom_path(mapping=aes(y=Ac_Index_MU1_3,x=
   scale_x_continuous(limits = c(0,50), expand = c(0, 0)) +
   geom_path(data=D[!is.na(D$MA_As1_3),],mapping=aes(y=MA_As1_3,x=Year),color="red") +
   geom_path(data=D[!is.na(D$loess_As1_3),],mapping=aes(y=loess_As1_3,x=Year),color="blue") +
-  geom_hline(yintercept=1000, linetype="dashed", color = "orange",size=1)
+  geom_hline(yintercept=750, linetype="dashed", color = "orange",size=1)
 
 ########################################################################################################################
 # Plots (MU1)
 ########################################################################################################################
 
 # 1. MU1 PS Catch
-ggplot(D[!(is.na(D$PS_Catch_MU1)),],aes(y=PS_Catch_MU1,x=Year)) + geom_path() + theme_classic() + labs(x='Year', y="MU1 Purse Seine Landings (kt)") + expand_limits(y=0)
+ggplot(D[!(is.na(D$PS_Catch_MU1)),],aes(y=PS_Catch_MU1,x=Year)) + geom_path() + theme_classic() + labs(x='Year', y="MU1 Purse Seine Catch (kt)") + expand_limits(y=0)
 
 # 2. MU1 PS Effort
 ggplot(D[!(is.na(D$PS_Effort_MU1)),],aes(y=PS_Effort_MU1,x=Year)) + geom_path() + theme_classic() + labs(x='Year', y="MU1 Purse Seine Effort (trips)") + expand_limits(y=0)
--- Exercise 2/ex2.R
+++ Exercise 2/ex2.R
@@ -29,10 +29,10 @@ BHa <- 4*h/(phi0*(1-h)) # estimated Beverton-Holt a
 BHb <- 1/R0*(BHa-1/phi0) # estimated Beverton-Holt b
 
 # Unfished equilibrum SSB0
-SSB0 <- R0/(BHa-BHb*R0)
+SSB0 <- R0*phi0
 
 ########################################################################################################################
-# Calculate equilibrium SSBmsy
+# Calculate equilibrium SSBmsy and ratio of 
 ########################################################################################################################
 
 # MSYcalc: function from functions.R returns a list with Fmsy, msy, SSBmsy from M=M, waa=weight-at-age, mat=maturity-at-age, sel=vulnerability-at-age, Beverton-Holt a and b
@@ -44,27 +44,30 @@ Fmsy <- calc$Fmsy
 msy <- calc$msy
 SSBmsy <- calc$SSBmsy
 
+#Ratio of SSBmsy to SSB0
+
+SSBmsy/SSB0
 
 ########################################################################################################################
 # Plots from data frame AA
 ########################################################################################################################
 
 # Plots for Weight-at-age, Maturity-at-age, and vulnerability-at-age
-ggplot(AA,aes(y=w_age/1000,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Weight (kg)") + expand_limits(y=0) + expand_limits(x=0) 
-ggplot(AA,aes(y=m_age,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Maturity") + expand_limits(y=0) + expand_limits(x=0) 
-ggplot(AA,aes(y=v_age,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Vulnerability") + expand_limits(y=0) + expand_limits(x=0) 
+ggplot(AA,aes(y=w_age/1000,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Weight (kg)") + expand_limits(y=0) + expand_limits(x=0) + scale_x_continuous(breaks=0:11)
+ggplot(AA,aes(y=m_age,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Maturity") + expand_limits(y=0) + expand_limits(x=0) + scale_x_continuous(breaks=0:11)
+ggplot(AA,aes(y=v_age,x=age)) + geom_path() + theme_classic() + labs(x="Age", y="Vulnerability") + expand_limits(y=0) + expand_limits(x=0) + scale_x_continuous(breaks=0:11)
 
 ########################################################################################################################
 # Plots from data frame D
 ########################################################################################################################
 #Stock recruitment pairs (labels are years)
-ggplot(D[!is.na(D$Rec),],aes(y=Rec,x=SSB,label=Year)) + geom_point() + theme_classic() + labs(x="SSB (kt)", y="Recruitment (10^9)") + expand_limits(y=0) + expand_limits(x=0) + geom_text(mapping=aes(y=Rec,x=SSB,label=Year),nudge_y = 0.5,size=2) 
+ggplot(D[!is.na(D$Rec),],aes(y=Rec,x=SSB,label=Year)) + geom_point(colour="blue") + theme_classic() + labs(x="SSB (kt)", y="Recruitment (10^9)") + expand_limits(y=c(0,12.5)) + expand_limits(x=0) + geom_text(mapping=aes(y=Rec,x=SSB,label=Year),nudge_y = 0.5,size=3) 
 
 #Stock recruitment pairs and model fit
-ggplot() + geom_point(D[!is.na(D$Rec),],mapping=aes(y=Rec,x=SSB)) + theme_classic() + labs(x="SSB (kt)", y="Recruitment (10^9)") + expand_limits(y=0) + expand_limits(x=0) + geom_function(fun=function(x) BHa*x/(1+BHb*x)) 
+ggplot() + geom_point(D[!is.na(D$Rec),],mapping=aes(y=Rec,x=SSB),colour="blue") + theme_classic() + labs(x="SSB (kt)", y="Recruitment (10^9)") + expand_limits(y=c(0,12.5)) + expand_limits(x=0) + geom_function(fun=function(x) BHa*x/(1+BHb*x)) 
 
 #Historical Recruitment
-ggplot(D[!is.na(D$Rec),],aes(y=Rec,x=Year)) + geom_path() + theme_classic() + labs(x="Year", y="Recruitment (10^9)") + expand_limits(y=0) 
+ggplot(D[!is.na(D$Rec),],aes(y=Rec,x=Year)) + geom_path() + theme_classic() + labs(x="Year", y="Recruitment (10^9)") + expand_limits(y=0) + expand_limits(x=50) 
 
 #Historical SSB 
 ggplot(D,aes(y=SSB,x=Year)) + geom_path() +theme_classic() + labs(x="Year", y="SSB (kt)") + expand_limits(y=0)
@@ -75,14 +78,15 @@ ggplot(D,aes(y=Catch,x=Year)) + geom_path() + theme_classic() + labs(x="Year", y
 #Historical F
 ggplot(D,aes(y=f,x=Year)) + geom_path() + theme_classic() + labs(x="Year", y="F") + expand_limits(y=0) 
 
-#Plot Equilibrium SSB0 (blue), Equilibrium SSBmsy (green)
-ggplot(D) + 
-  geom_path(mapping=aes(y=SSB,x=Year)) +
-  theme_classic() + labs(x="Year", y="SSB (kt)") + expand_limits(y=0) +
-  geom_hline(yintercept=SSB0, linetype="dashed", color = "blue") + 
+#Plot Equilibrium SSB0 (purple), Equilibrium SSBmsy (green)
+ggplot(D) + geom_path(mapping=aes(y=SSB,x=Year)) + theme_classic() + labs(x="Year", y="SSB (kt)") + expand_limits(y=0) +
+  geom_hline(yintercept=SSB0, color = "purple") + 
   geom_hline(yintercept=SSBmsy, color = "green") 
 
-
+#Plot Historical F and Equilibrium Fmsy (green)
+ggplot(D,aes(y=f,x=Year)) + geom_path() + theme_classic() + labs(x="Year", y="F") + expand_limits(y=0) +
+  geom_hline(yintercept=Fmsy, color = "green")  
+  
 
 #Plot Acoustic index years 25-50
   #Add x yr moving average (example 3 years)
@@ -100,6 +104,9 @@ ggplot(D[!is.na(D$Acoustic_Index),]) + geom_path(mapping=aes(y=Acoustic_Index,x=
   geom_path(data=D[!is.na(D$MA_Index),],mapping=aes(y=MA_Index,x=Year),color="red") +
   geom_path(data=D[!is.na(D$loess_Index),],mapping=aes(y=loess_Index,x=Year),color="blue") 
 
+
+#Plot relative exploitation rate [example: catch/(smoothed index)] years 25-50
+ggplot(D[!is.na(D$Acoustic_Index),]) + geom_path(mapping=aes(y=Catch/loess_Index,x=Year)) + theme_classic() + labs(x="Year", y="Relative Exploitation Rate") + expand_limits(y=0,x=0) 
 
 
 #Plot stock status indicator (example: suppose LRP = 30% SSB0)
